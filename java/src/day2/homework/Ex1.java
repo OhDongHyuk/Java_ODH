e60555e73ed3f3b34e12c1ff9ec4835963a7af3d
OhDongHyuk <daed124@naver.com>
2023-05-31 17:43:16 +0900

    숙제

--- java/src/day2/homework/Ex1.java
+++ java/src/day2/homework/Ex1.java
@@ -1,9 +1,18 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-        
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.println("두 정수와 산술 연산자를 입력하세요.");
+		
+        String str1 = sc.nextLine();
+        System.out.println(str1);
+		
+        sc.close();
 	}
 
 }
--- java/src/day2/homework/Ex2.java
+++ java/src/day2/homework/Ex2.java
@@ -1,9 +1,30 @@
 package day2.homework;
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in); 
+		
+		System.out.println("두 정수를 입력하세요.");
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		        
+        int sum = num1 + num2;
+        int sub = num1 - num2;
+        int mul = num1 * num2;
+        double div = (double) num1 / num2;
+        int mod = num1 % num2;
+        
+        System.out.println("" + num1 + '+' + num2 + "=" + sum);
+        System.out.println("" + num1 + '-' + num2 + "=" + sub);
+        System.out.println("" + num1 + '*' + num2 + "=" + mul);
+        System.out.println("" + num1 + '/' + num2 + "=" + div);
+        System.out.println("" + num1 + '%' + num2 + "=" + mod);
+
+        sc.close();
 
 	}
 
