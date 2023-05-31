a5638ef01cf337a31e7ed0e7397c1bcd5fca3764
OhDongHyuk <daed124@naver.com>
2023-05-31 12:41:15 +0900

    자바 오버플로우/언더프로우 예제

---
+++ java/src/day2/variable/OverFlowEx.java
@@ -0,0 +1,14 @@
+package day2.variable;
+
+public class OverFlowEx {
+
+	public static void main(String[] args) {
+		byte num = 127;
+		++num;//num에 1을 증가
+		System.out.println(num);//오버플로우가 발생
+		num = -128;
+		--num;//num에 1을 감소
+		System.out.println(num);//언더플로우가 발생
+	}
+
+}
