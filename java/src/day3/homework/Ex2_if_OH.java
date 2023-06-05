32dd9d86591b3842818f5d6bf2ecd1feffc62c99
OhDongHyuk <daed124@naver.com>
2023-06-05 09:00:43 +0900

    11

--- java/src/day3/homework/Ex2_if_OH.java
+++ java/src/day3/homework/Ex2_if_OH.java
@@ -2,7 +2,7 @@ package day3.homework;
 
 import java.util.Scanner;
 
-public class Ex2_if {
+public class Ex2_if_OH {
 
 	public static void main(String[] args) {
 		/*성별(M:남성,W:여성)를 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하세요. if문 이용
