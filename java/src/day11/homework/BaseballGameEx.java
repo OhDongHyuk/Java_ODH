90331234104589b7499823d64890479204a568a7
OhDongHyuk <daed124@naver.com>
2023-06-14 14:54:25 +0900

    숙제중

---
+++ java/src/day11/homework/BaseballGameEx.java
@@ -0,0 +1,79 @@
+package day11.homework;
+
+import java.util.Scanner;
+import java.util.zip.CRC32;
+
+public class BaseballGameEx {
+
+	public static void main(String[] args) {
+		// 1 ~ 9 사이의 중복되지 않은 세 수를 맞추는 게임. 3S까지 하는 것
+		// 규칙 :것
+		// B : 같은 숫자가 있지만 위치가 다름
+		// S : 같은 숫자가 있고 위치가 같음
+		// 30 : 일치하는 숫자가 하나도 없음
+		// 예 :
+		// 랜덤 숫자 : 1 9 5
+		// 사용자 : 1 4 9 => 1S 1B
+		// 사용자 : 2 7 4 => 30
+		
+		int [] arr = new int[3];
+				
+		createRandomArray(1, 9, arr);
+		printArr(arr);
+		System.out.println("");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Input numbers : " );
+		for(int i = 0; i < 3; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.print("User : ");
+		for(int i = 0; i < 3; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+		for(int i = 0; i < 3; i++) {
+			if(random == arr[i]) {
+				System.out.println("S");
+			}
+		}
+		
+		sc.close();
+	}
+	public static void printArr(int arr[]) {
+		int i;
+		System.out.print("Random numbers : ");
+		for(i = 0; i < arr.length ; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+	public static boolean contains2(int arr[], int num, int count) {
+		for(int i = 0; i < count; i++) {
+			if(num == arr[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static void createRandomArray(int min, int max, int arr[]) {
+		//배열이 생성되어 있지 않으면(배열이 null이면) 메소드 종료
+		if(arr == null) {
+			return;
+		}
+		//랜덤으로 만들어지는 수의 개수가 배열의 크기보다 작으면 메소드를 종료
+		//1~3 : 3개가 4개짜리 배열에 중복되지 않게 들어갈 수 없음
+		if(max - min + 1 < arr.length) {
+			return;
+		}
+		//배열이 생성
+		int count = 0;
+		while(count < 3) {
+			int random = (int)(Math.random() * (max - min + 1) + min);
+			if(!contains2(arr, random, count)) {
+				arr[count] = random;
+				count++;
+			}
+		}
+	}
+}
