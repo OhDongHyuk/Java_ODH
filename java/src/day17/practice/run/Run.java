2eb347ac1d3ad9d8ec13cd90d71bd4322d2af09b
OhDongHyuk <daed124@naver.com>
2023-06-23 08:51:33 +0900

    11

--- java/src/day17/practice/run/Run.java
+++ java/src/day17/practice/run/Run.java
@@ -1,12 +1,12 @@
 package day17.practice.run;
 
-import day15.practice.shop.controller.ShopManager;
+import day17.practice.controller.ShopController;
 
 public class Run {
 
 	public static void main(String[] args) {
 		//ShopManager를 이용하여 제품을 관리하는 프로그램을 실행
-		ShopManager shopManager = new ShopManager();
+		ShopController shopManager = new ShopController();
 		shopManager.run();
 	}
 
