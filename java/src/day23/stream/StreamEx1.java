401ea91924c38686b55d12ba5e5da02b2c6fc730
OhDongHyuk <daed124@naver.com>
2023-07-04 08:56:23 +0900

    11

--- java/src/day23/stream/StreamEx1.java
+++ java/src/day23/stream/StreamEx1.java
@@ -9,17 +9,18 @@ import java.util.Scanner;
 public class StreamEx1 {
 
 	public static void main(String[] args) {
+		
 		/* test.txt는 상대 경로. 기준은 프로젝트 폴더가 기준
-		 * */
-
+		 * */ 
 		/*FileInputStream fis = null;
 		
 		try {
 			fis = new FileInputStream("test.txt");
+			
 		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
+			System.out.println("File not found!");
 		} finally {
-			//프로그램 종료 전에 사용했던 파일을 닫아주는 작업
+			//프로그램 종료 전 사용했던 파일을 닫아줌
 			if(fis != null) {
 				try {
 					fis.close();
@@ -27,35 +28,42 @@ public class StreamEx1 {
 					e.printStackTrace();
 				}
 			}
-		}*/
+		}
+		*/
 		Scanner sc = new Scanner(System.in);
-		try(FileOutputStream fos = new FileOutputStream("text.txt")) {
-			for(int i = 0; i < 5; i++) {
-				System.out.print("Input : ");
+		//파일을 쓰는 예제
+		try(FileOutputStream fos = new FileOutputStream("test.txt")){
+			for(int i = 0; i<5; i++) {
+				System.out.print("input : ");
 				char ch = sc.next().charAt(0);
 				fos.write(ch);
 			}
+			
 		} catch (FileNotFoundException e1) {
 			System.out.println("File not found!!");
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
-		
-		
-		try(FileInputStream fis2 = new FileInputStream("text.txt")) {
+		//파일을 읽는 예제
+		try(FileInputStream fis2 = new FileInputStream("test.txt")){
 			int data;
-			while((data = fis2.read()) != -1) {
+			while((data = fis2.read()) != -1 ) {
 				System.out.println((char)data);
 			}
-		}catch (FileNotFoundException e) {
-			System.out.println("File not found");
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		System.out.println(1);
 		System.out.println((int)'1');
-
-		}
+	}
 
 }
+
+
+
+
+
+
--- java/src/day23/stream/StreamEx2.java
+++ java/src/day23/stream/StreamEx2.java
@@ -9,55 +9,31 @@ public class StreamEx2 {
 
 	public static void main(String[] args) {
 		//파일에 문자들을 씀
-		try(FileWriter fw = new FileWriter("text.txt")) {
-			for(int i = 0; i < 5; i++) {
-				fw.write("안녕하세요" + i + "\n");
+		try(FileWriter fw = new FileWriter("test.txt")){
+			for(int i = 0; i<5; i++) {
+				fw.write("안녕하세요"+i+"\n");
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		//파일에 쓰여 있는 문자들을 읽어옴
-		try(FileReader fr = new FileReader("text.txt")) {
-			char ch[] = new char[10];
+		try(FileReader fr = new FileReader("test.txt")){
+			char ch[] = new char[1024];
 			int len;
 			while((len = fr.read(ch)) != -1) {
-				for(int i = 0; i < len; i++) {
+				/*
+				for(int i = 0; i<len; i++) {
 					System.out.print(ch[i]);
-				}
+				}*/
+				System.out.print(ch);
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found!!");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
